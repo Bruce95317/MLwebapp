a70253f7e7e34dd4f508b2f5f4cb9cc8b87be903
Bruce95317 <yyc95317@outlook.com>
2021-04-07 00:32:08 +0800

    first commit

--- mlweb/Scripts/activate.bat
+++ mlweb/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Desktop\ML webapp (diabetic)\mlweb
+set VIRTUAL_ENV=C:\Users\user\Desktop\MLwebapp\mlweb
 
 if not defined PROMPT set PROMPT=$P$G
 
